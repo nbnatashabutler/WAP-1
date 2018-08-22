626b7dc2e45c76b67cf9eba5f84f2815a8910197
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-22 13:58:39 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -5,3 +5,11 @@ downloadingImage.onload = function(){
     image.src = this.src;   
 };
 downloadingImage.src = "https://s19.postimg.cc/jk17ed7mr/6479b2.png";
+
+
+var myArray = ["one", "two", "three"];
+  
+for (var i = 0; i < myArray.length; i++) {
+    document.writeln(myArray[i]);
+}
+
