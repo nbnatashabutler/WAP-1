cf7ddeeda903a37b74791cf8b63e4884a2123b1f
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-22 13:41:24 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,8 +1,7 @@
-var data = {
-  "images": [{
-    "bannerImg1": "https://triplebyte-cats.s3.amazonaws.com/jiji.png"
-  },
-  {"bannerImg1" : "https://triplebyte-cats.s3.amazonaws.com/lime.jpg"
-  },{"bannerImg1" : "https://triplebyte-cats.s3.amazonaws.com/space.jpg"
-  }]
+
+var image = document.images[0];
+var downloadingImage = new Image();
+downloadingImage.onload = function(){
+    image.src = this.src;   
 };
+downloadingImage.src = "https://s19.postimg.cc/jk17ed7mr/6479b2.png";
