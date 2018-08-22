e758c21aa7849dcdfa9c1474c0e8d1c9ab8fb1bf
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-22 14:15:30 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -6,10 +6,24 @@ downloadingImage.onload = function(){
 };
 downloadingImage.src = "https://s19.postimg.cc/jk17ed7mr/6479b2.png";
 
+//for loop = do this for that many times explicitly
 
-var myArray = ["one", "two", "three"];
+var myArray = ["first item in the array", "second item in the array", "third item in the array"];
   
 for (var i = 0; i < myArray.length; i++) {
     document.writeln(myArray[i]);
 }
 
+//while loop = do this until that is not true
+
+var count = 0;
+        
+while (count < 10) {
+    document.writeln("you will see this 9 times");
+ 
+    count++;
+}
+
+do {
+    document.writeln("This code will run once!");
+}
