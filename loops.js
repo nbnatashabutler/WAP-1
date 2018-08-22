a7de23b668f3d66645c551dc545219d812bf7ef3
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-22 13:35:24 -0700

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,8 @@
+var data = {
+  "images": [{
+    "bannerImg1": "https://triplebyte-cats.s3.amazonaws.com/jiji.png"
+  },
+  {"bannerImg1" : "https://triplebyte-cats.s3.amazonaws.com/lime.jpg"
+  },{"bannerImg1" : "https://triplebyte-cats.s3.amazonaws.com/space.jpg"
+  }]
+};
