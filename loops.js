3a1291323d242e3f814ae2b046164458eb63fbd4
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-23 12:56:23 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -40,3 +40,62 @@ while (jobs < reqlimit) {
     job++;
     document.writeln("There's room for " + (reqlimit - job) + " more jobs.");
 }
+
+//switch case 1
+// Set the student's grade
+const grade = 87;
+
+switch (true) {
+    // If score is 90 or greater
+    case grade >= 90:
+        document.writeln("A");
+        break;
+    // If score is 80 or greater
+    case grade >= 80:
+        document.writeln("B");
+        break;
+    // If score is 70 or greater
+    case grade >= 70:
+        document.writeln("C");
+        break;
+    // If score is 60 or greater
+    case grade >= 60:
+        document.writeln("D");
+        break;
+    // Anything 59 or below is failing
+    default:
+        document.writeln("F");
+}
+
+// switch case 2
+
+const month = new Date().getMonth();
+
+switch (month) {
+    // January, February, March
+    case 0:
+    case 1:
+    case 2:
+        document.writeln("Q1");
+        break;
+    // April, May, June
+    case 3:
+    case 4:
+    case 5:
+        document.writeln("Q2");
+        break;
+    // July, August, September
+    case 6:
+    case 7:
+    case 8:
+        document.writeln("Q3");
+        break;
+    // October, November, December
+    case 9:
+    case 10:
+    case 11:
+        document.writeln("Q4");
+        break;
+    default:
+        document.writeln("Ooops. Check back later.");
+}
