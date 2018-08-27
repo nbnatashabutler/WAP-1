6d7e2651bc905db1951f5edbd18f265e17afdca0
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-27 14:20:45 -0700

    Create capture.js

---
+++ capture.js
@@ -0,0 +1,5 @@
+for(let elem of document.querySelectorAll('*')) {
+    elem.addEventListener("click", e => alert(`Capturing: ${elem.tagName}`), true);
+    elem.addEventListener("click", e => alert(`Bubbling: ${elem.tagName}`));
+  }
+//javascript.info/bubling-and-capturing
