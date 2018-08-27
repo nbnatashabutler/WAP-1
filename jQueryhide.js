ec2789af333e3e288db8a3c10f3db88b62dcf7fc
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-27 14:41:43 -0700

    Create jQueryhide.js

---
+++ jQueryhide.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".btn1").click(function(){
+        $("p").hide();
+    });
+    $(".btn2").click(function(){
+        $("p").show();
+    });
+});
+
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
