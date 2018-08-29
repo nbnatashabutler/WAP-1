d7762260773f46fb6627716fd8a7bea2a974ca45
Natasha Butler <nbnatashabutler@gmail.com>
2018-08-29 13:00:10 -0700

    Update selectors.js

--- selectors.js
+++ selectors.js
@@ -5,3 +5,7 @@ $(document).ready(function(){
 $(document).ready(function(){
     $("p:nth-child(3)").css("background-color", "green");
 });
+
+$(document).ready(function(){
+  $("p:eq(3)").css("background-color","red");
+});
